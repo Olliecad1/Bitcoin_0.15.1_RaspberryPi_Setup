1528e6ad41fc7a739b24c73b02a08297e954d7ed
olliecad1 <officialgamermorris@gmail.com>
2018-02-20 16:09:52 +0000

    some_message

---
+++ Installing_Bitcoin_0.15.1.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# Install updates
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install dependencies for Bitcoin Core (not the GUI)
+sudo apt-get install autoconf libevent-dev libtool libssl-dev libboost-all-dev libminiupnpc-dev -y
+
+# Install dependencies for Bitcoin QT (GUI)
+sudo apt-get install qt4-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev -y
+
+
+# Setup Swap file
+sudo sed -i '16s/.*/CONF_SWAPSIZE=2048/g' /etc/dphys-swapfile
+sudo dphys-swapfile setup
+sudo dphys-swapfile swapon
+
+mkdir ~/bin
+cd ~/bin
+
+# Install the Berkeley DB 4.8
+wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+tar -xzvf db-4.8.30.NC.tar.gz
+cd db-4.8.30.NC/build_unix/
+
+# Configuring the system and installing Berkeley DB
+../dist/configure --enable-cxx
+make -j4
+sudo make install
+
+# Installing Bitcoin 0.15.1
+cd ~/bin
+git clone -b v0.15.1 https://github.com/bitcoin/bitcoin
+cd bitcoin/
+./autogen.sh
+./configure CPPFLAGS="-I/usr/local/BerkeleyDB.4.8/include -O2" LDFLAGS="-L/usr/local/BerkeleyDB.4.8/lib" --enable-upnp-default --with-gui=qt4
+make -j2
+sudo make install
+
+# Setting up Bitcoin Data Folder
+mkdir /home/pi/.bitcoin/
+touch ~/.bitcoin/bitcoin.conf
+printf 'listen=1\nserver=1\ndaemon=0\ntestnet=0\nmempoolexpiry=72\nmaxmempool=300\nmaxorphantx=100\nlimitfreerelay=10\nminrelaytxfee=0.0001\nmaxconnections=40\nrpcuser=BITCOIN_USER\nrpcpassword=BITCOIN_NEWPYTHON123751\nrpcport=8332' >> bitcoin.conf
+
+
+
---
+++ README.md
@@ -0,0 +1,20 @@
+## Bash script for setting up Bitcoin on Raspberry Pi
+
+Downloading the bash script
+
+``` 
+git clone https://github.com/Olliecad1/Bitcoin_0.15.1_RaspberryPi_Setup/blob/master/Installing_Bitcoin_0.15.1.sh
+```
+
+Executing the Bash Script
+
+```
+sudo chmod +x ./Installing_Bitcoin_0.15.1.sh
+```
+
+Running the Script
+
+``` 
+./Installing_Bitcoin_0.15.1.sh
+```
+# if you wish to tip me this is my address 1M7HN2wh32YU4AMfMatrAnCcdyfmuow7V3
